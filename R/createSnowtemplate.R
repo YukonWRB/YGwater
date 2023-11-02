cc24ae12f73902b18981fba8e1f62b70dc0a3b0c
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2023-11-02 17:43:47 +0000

    Snow template deals with missing sites and has additional features

--- R/createSnowtemplate.R
+++ R/createSnowtemplate.R
@@ -14,14 +14,13 @@
 #' @export
 #'
 
-# TODO: (3) add conditional formatting to summary sheet to colour when QAQC is complete
-# TODO: (4) Link in summary to sheet
+#TODO: add option to create workbook for all circuits
 
 # # For testing
 # path = "C:/Users/estewart/Documents/R/Projects/"
 # circuit = "Carmacks"
 # target_date = "2023-03-01"
-#createSnowtemplate(target_date = "2023-03-01", path = "C:/Users/estewart/Documents/R/Projects/", circuit = "Carmacks")
+#createSnowtemplate(target_date = "2023-04-01", path = "C:/Users/estewart/Documents/R/Projects/", circuit = "Ross")
 
 
 createSnowtemplate <- function(target_date, path, circuit) {
@@ -33,7 +32,7 @@ if (circuit == "Carmacks") {
   courses <- c("Casino Creek", "MacIntosh", "Mount Berdoe", "Mount Nansen", "Satasha Lake", "Williams Creek")
 }
 if (circuit == "Dawson") {
-  courses <- c("Blackstone River", "Eagle Plains", "Eagle River", "Grizzly Creek", "King Solomon Dome", "Midnight Dome", "Ogilvie River", "Riff's Ridge")
+  courses <- c("Blackstone River", "Eagle Plains", "Eagle River", "Grizzly Creek", "King Solomon Dome", "Midnight Dome", "Ogilvie River", "Riffs Ridge")
 }
 if (circuit == "HJ") {
   courses <- c("Beaver Creek", "Burwash Airstrip", "Chair Mountain", "Haines Junction Farm", "Summit")
@@ -42,8 +41,9 @@ if (circuit == "KluaneP") {
   courses <- c("Alder Creek")
 }
 if (circuit == "Mayo") {
-  courses <- c("Calumet", "Mayo Airpot A", "Mayo Airport B", "Mayo Airport C")
+  courses <- c("Calumet", "Mayo Airport A", "Mayo Airport B", "Mayo Airport C") #
 }
+# North slope locations don't currently exist in database
 if (circuit == "NorthSlope") {
   courses <- c("AK Border", "Herschel Island", "Komakuk", "NWT/YK Border", "Stakes", "Shingle Point")
 }
@@ -53,28 +53,33 @@ if (circuit == "OldCrow") {
 if (circuit == "PellyFarm") {
   courses <- c("Pelly Farm")
 }
+# Snow scale/pillow locations not in db. Twin Creeks not in hydromet, but in snow db
 if (circuit == "Ross") {
-  courses <- c("Bonnet Plume Lake", "Burns Lake", "Edwards Lake", "Finlayson Airstrip", "Ford Lake", "Fuller Lake", "Hoole River", "Jordan Lake", "Plata Airstrip", "Rackla Lake", "Rose Creek", "Russell Lake", "Tintina Airstrip", "Twin Creeks A", "Twin Creeks B", "Twin Creeks B Snow Scale", "Withers Lake", "Withers Pillow", "Withers Scale")
+  courses <- c("Bonnet Plume Lake", "Burns Lake", "Edwards Lake", "Finlayson Airstrip", "Ford Lake", "Fuller Lake", "Hoole River", "Jordan Lake", "Plata Airstrip", "Rackla Lake", "Rose Creek", "Russell Lake", "Tintina Airstrip", "Twin Creeks B", "Withers Lake", "Twin Creeks A", "Twin Creeks B Snow Scale", "Withers Pillow", "Withers Scale") #
 }
+# Snow scale/pillow locations not in db.
 if (circuit == "SLakes") {
-  courses <- c("Atlin (B.C)", "Log Cabin (B.C.)", "Montana Mountain", "Tagish Snow Scale", "Tagish Snow Pillow", "Tagish")
+  courses <- c("Atlin (B.C)", "Log Cabin (B.C.)", "Montana Mountain", "Tagish", "Tagish Snow Scale", "Tagish Snow Pillow")
 }
 if (circuit == "Teslin") {
   courses <- c("Meadow Creek", "Morley Lake", "Pine Lake Airstrip")
 }
+# Hyland River does not exist in hydromet db, but is in snow db
 if (circuit == "Watson") {
-  courses <- c("Frances River", "Hyland River", "Hyland River B", "Hyland Snow Scale", "Watson Lake Airport")
+  courses <- c("Frances River", "Hyland River B", "Hyland Snow Scale", "Watson Lake Airport", "Hyland River") #
 }
+# Buckbrush snow scales is not in db. Whitehorse Airport A is actually Whitehorse Airport A
 if (circuit == "WRB") {
-  courses <- c("Buckbrush Snow Scales", "Mt McIntyre B", "Whitehorse Airport A", "Whitehorse Airport B")
+  courses <- c("Buckbrush Snow Scales", "Mt McIntyre B", "Whitehorse Airport", "Whitehorse Airport B")
 }
 if (circuit == "YEC") {
   courses <- c("Aishihik Lake", "Canyon Lake")
 }
 
 #### ------------ Add sheet for each snow course of the circuit ----------- ####
-# Add _ to worksheet names
+# Add _ to worksheet names and remove '
   courses2 <- gsub(" ", "_", courses)
+  courses2 <- gsub("'", "", courses2)
 # Clone worksheets and fill in
 for (c in 1:length(courses2)) {
   # Clone worksheet
@@ -93,14 +98,19 @@ openxlsx::removeWorksheet(template, "Sheet1")
   summary <- SWE_station(year = as.numeric(substr(target_date, start = 1, stop = 4)),
                          month = as.numeric(substr(target_date, start = 7, stop = 7)),
                          return_missing = TRUE)
-  # Subset to locations of interest
-  summary <- summary[summary$location_name %in% courses, ]
+  # Subset to locations of interest and columns of interest
+  summary <- summary[summary$location_name %in% courses, c("location_name", "location_id", "swe_prevyear", "swe_med")]
+  # Add locations to summary that are not in database
+    # Get courses that are not in summary, but in courses
+  locs <- setdiff(courses, summary$location_name)
+  summary <- rbind(summary, data.frame("location_name"=locs, "location_id"=rep(NA, times=length(locs)), "swe_prevyear"=rep(NA, times=length(locs)), "swe_med"=rep(NA, times=length(locs))))
+
   # Put into alphabetical order
   summary <- summary[order(summary$location_name), ]
 
   # Add data to summary worksheet
-  openxlsx::writeData(template, sheet = "Summary", x = summary[1:3], xy = c(1,3), colNames = FALSE)
-  openxlsx::writeData(template, sheet = "Summary", x = summary[6:7], xy = c(7,3), colNames = FALSE)
+  openxlsx::writeData(template, sheet = "Summary", x = summary[1:2], xy = c(1,3), colNames = FALSE)
+  openxlsx::writeData(template, sheet = "Summary", x = summary[3:4], xy = c(7,3), colNames = FALSE)
   # Add formula to get values from other sheets
   # Sample date
   for (s in 1:length(courses)) {
@@ -112,11 +122,11 @@ openxlsx::removeWorksheet(template, "Sheet1")
   }
   # Density
   for (s in 1:length(courses)) {
-    openxlsx::writeFormula(template, sheet="Summary", x=paste0("='", courses2[s], "'!H25"), startCol=5, startRow=2+s)
+    openxlsx::writeFormula(template, sheet="Summary", x=paste0("=IF('", courses2[s], "'!D9=", '"bulk", ', "'", courses2[s], "'!H23, '", courses2[s], "'!H25)"), startCol=5, startRow=2+s)
   }
   # SWE
   for (s in 1:length(courses)) {
-    openxlsx::writeFormula(template, sheet="Summary", x=paste0("='", courses2[s], "'!G25*10"), startCol=6, startRow=2+s)
+    openxlsx::writeFormula(template, sheet="Summary", x=paste0("=IF('", courses2[s], "'!D9=", '"bulk", ', "'", courses2[s], "'!G23*10, '", courses2[s], "'!G25*10)"), startCol=6, startRow=2+s)
   }
   # SWE ratio
   for (s in 1:length(courses)) {
@@ -128,18 +138,18 @@ openxlsx::removeWorksheet(template, "Sheet1")
                           x = paste0("=HYPERLINK(", '"#', "'", courses2[s], "'", '!D4", "', courses[s], '")'),
                            startCol=10, startRow=2+s)
   }
-
   # Add hyperlink to each page
   for (s in 1:length(courses2)) {
-    # openxlsx::writeFormula(template, sheet=courses2[s],
-    #                        x = paste0("=HYPERLINK(", '"#', "'Summary'", '!A', 2+s, '", "', courses[s], '")'),
-    #                        startCol=2, startRow=1)
     openxlsx::writeFormula(template, sheet=courses2[s],
                            x = paste0("=HYPERLINK(", '"#', "'Summary'", '!A', 2+s, '", "Link to summary sheet")'),
                            startCol=2, startRow=1)
   }
+  # Add QAQC yes/no
+  for (s in 1:length(courses)) {
+    openxlsx::writeFormula(template, sheet="Summary", x=paste0("=IF(ISBLANK('", courses2[s], "'!L23), ",'"no", "yes")'), startCol=11, startRow=2+s)
+  }
 
 # Write new template (template_test)
-openxlsx::saveWorkbook(template, file=paste0(path, "template_test.xlsx"), overwrite = TRUE)
+openxlsx::saveWorkbook(template, file=paste0(path, paste0(circuit, "_", target_date, ".xlsx")), overwrite = TRUE)
 
 }
