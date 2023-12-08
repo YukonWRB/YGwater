2dfcfb153a4a0dfc16c840115c7102086f535f23
EmilieStewartJones <e.stewart-jones@hotmail.ca>
2023-12-08 18:55:16 +0000

    Fixed bugs in combineWeather

--- R/combineWeather.R
+++ R/combineWeather.R
@@ -24,6 +24,13 @@
 # Subset to months of interest currently doesn't work because all dates are filled in after subset. Need to think of a way to get around this.
 # Fill in remaining gaps with simple interpolation?
 # Fill out roxygen
+# stations = list("2101310", "2101300", "2101400")
+# start='2010-01-01'
+# end='2013-12-03'
+# variables=c("mean_temp", "total_precip", "total_rain", "total_snow", "min_temp", "max_temp")
+# months=NULL
+
+# test <- combineWeather(stations = list("2101310", "2101300", "2101400", "2101415", "2101303"), start='1940-01-01', end='2023-11-01', variables=c("mean_temp", "total_precip", "total_rain", "total_snow", "min_temp", "max_temp"), months=NULL)
 
 combineWeather <- function(stations, start, end, variables, months=NULL) {
 
@@ -31,110 +38,127 @@ combineWeather <- function(stations, start, end, variables, months=NULL) {
     months <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
   }
 
-  #---------------------------------- Get data ----------------------------------#
-  if (is.data.frame(stations[[1]])) {
-    station1 <- stations[[1]]
-  } else if (is.character(stations[[1]])) {
-    station1 <- getWeather(
-      station = stations[[1]],
-      start = start,
-      end = end,
-      interval = "day",
-      save_path = NULL
-    )
-  } else {warning("The objects in the stations parameter must be either a station id (character string) or a dataframe")}
+#---------------------------------- Get data ----------------------------------#
 
-  if (is.data.frame(stations[[2]])) {
-    station2 <- stations[[2]]
-  } else if (is.character(stations[[2]])) {
-    station2 <- getWeather(
-      station = stations[[2]],
-      start = start,
-      end = end,
-      interval = "day",
-      save_path = NULL
-    )
-  } else {warning("The objects in the stations parameter must be either a station id (character string) or a dataframe")}
+  ## For each station
+  for (s in 1:length(stations)) {
 
-  # Subset to variables of interest and months of interest
-  station1 <- station1 %>% dplyr::select(c("date", tidyselect::all_of(variables))) %>%
-    dplyr::filter(lubridate::month(date) %in% months)
-
-  station2 <- station2 %>% dplyr::select(c("date", tidyselect::all_of(variables))) %>%
-    dplyr::filter(lubridate::month(date) %in% months)
-
-#-------------- Run for loop over every variable of interest ------------------#
-  # Create empty list
-  combined_data_list <- list()
-
-  for (v in variables) {
-    # Subset station dataframe to variable of interest
-    stn1 <- station1 %>% dplyr::select(c("date", v))
-    stn2 <- station2 %>% dplyr::select(c("date", v))
-
-    # Set standard column names
-    colnames(stn1) <- c("date", "variable")
-    colnames(stn2) <- c("date", "variable")
-
-    #------------------------------ Find overlap ------------------------------#
-    # Find overlap
-    overlap <- dplyr::inner_join(stn1, stn2, by='date')
-    # find earliest date that has the month and day +1 of max_d
-    overlap <- overlap %>%
-      dplyr::filter(date >
-                      min(
-                        date[grepl(
-                          paste0(
-                            substr(
-                              as.character(max(overlap$date) + 1),
-                              nchar(as.character(max(overlap$date) + 1)) - 5,
-                              nchar(as.character(max(overlap$date) + 1))
-                            ), "$"
-                          ), format(date, "-%m-%d"))])
+    # Get data
+    if (is.data.frame(s)) {
+      station <- stations[[s]]
+    } else if (is.character(stations[[s]])) {
+      station <- getWeather(
+        station = stations[[s]],
+        start = start,
+        end = end,
+        interval = "day",
+        save_path = NULL
       )
+    } else {warning("The objects in the stations parameter must be either a station id (character string) or a dataframe")}
 
-    #---------------------------- Calculate stats ----------------------------#
-    print(paste0("Mean at station 1:", mean(overlap$variable.x, na.rm=TRUE)))
-    print(paste0("Mean at station 2:", mean(overlap$variable.y, na.rm=TRUE)))
-    bias <- mean(overlap$variable.x, na.rm=TRUE) - mean(overlap$variable.y, na.rm=TRUE)
-    print(paste0("Bias: ", bias))
-    print(paste0("RMSE: ", sqrt(mean((overlap$variable.x-overlap$variable.y)^2, na.rm=TRUE))))
+    test2 <<- station
+    # Check if data exists for this time frame
+    if (nrow(station)==0) {
+      station <- station
+      warning(paste0("Station ", station, " does not have data for this time frame"))
+    } else {
+      # Subset to variables of interest and months of interest
+      station <- station %>% dplyr::select(c("date", tidyselect::all_of(variables))) %>%
+        dplyr::filter(lubridate::month(date) %in% months)
 
-    #---------------------------- Apply correction ----------------------------#
-    # Apply correction to all values of station 2
-    stn2$variable <- stn2$variable + bias
-
-    print("Correction applied")
-
-    #------------------------- Fill gaps in station 1 -------------------------#
-    # Where station 1 is missing data in range, fill in with corrected station 2
-    # Fill in all missing days in range for station 1 with NAs
-    all_dates <- seq(as.Date(start), as.Date(end), by = "days")
-    missing_dates <- as.Date(setdiff(all_dates, stn1$date))
-
-    if (length(missing_dates)>=1) {
-      missing_data <- data.frame(date = missing_dates, variable = NA)
-      stn1 <- dplyr::bind_rows(stn1[, c("date", "variable")], missing_data)
+      # Transform to long format
+      station <- station %>%
+        tidyr::pivot_longer(cols = variables,
+                            names_to = "variable",
+                            values_to = "value")
     }
 
-    # Replace NAs with corrected station 2
-    combined_data <- stn1 %>%
-      dplyr::left_join(stn2, by = "date", suffix = c("_stn1", "_stn2")) %>%
-      dplyr::mutate(variable = dplyr::coalesce(.data$variable_stn1, .data$variable_stn2)) %>%
-      dplyr::select(date, variable) %>%
-      dplyr::arrange(date)
+    # Replace in stations list
+    stations[[s]] <- station
 
-    print("Gaps filled")
+    test1 <<- stations
 
-    # Rename variable column with original name
-    colnames(combined_data) <- c("date", v)
-
-    # Add to list
-    combined_data_list <- c(combined_data_list, list(combined_data))
-
-    print("Added to list")
   }
 
-  return(combined_data_list)
+#--------------------- Run for loop over every station ------------------------#
+  # Create dataframe of final product which is
+  combined_stations <- stations[[1]]
+
+  for (s in 2:length(stations)) {
+
+    # Select station from list to be added
+    station2 <- stations[[s]]
+
+    if (nrow(station2)==0) {next}
+
+    # Run for loop over every variable of interest
+    combined_variables <- setNames(data.frame(matrix(ncol = 3, nrow = 0)), c("date", "value", "variable"))
+    for (v in variables) {
+
+      print(paste0("Combining variable '", v, "'"))
+
+      # Subset station dataframe to variable of interest
+      stn1 <- combined_stations[combined_stations$variable==v,]
+      stn2 <- station2[station2$variable==v,]
+
+      #----------------------------- Find overlap -----------------------------#
+      # Find overlap
+      overlap <- dplyr::inner_join(stn1, stn2, by='date')
+      # find earliest date that has the month and day +1 of max_d
+      overlap <- overlap %>%
+        dplyr::filter(date >
+                        min(
+                          date[grepl(
+                            paste0(
+                              substr(
+                                as.character(max(overlap$date) + 1),
+                                nchar(as.character(max(overlap$date) + 1)) - 5,
+                                nchar(as.character(max(overlap$date) + 1))
+                              ), "$"
+                            ), format(date, "-%m-%d"))])
+        )
+
+      #--------------------------- Calculate stats ---------------------------#
+      print(paste0("Mean at station 1:", mean(overlap$value.x, na.rm=TRUE)))
+      print(paste0("Mean at station 2:", mean(overlap$value.y, na.rm=TRUE)))
+      bias <- mean(overlap$value.x, na.rm=TRUE) - mean(overlap$value.y, na.rm=TRUE)
+      print(paste0("Bias: ", bias))
+      print(paste0("RMSE: ", sqrt(mean((overlap$value.x-overlap$value.y)^2, na.rm=TRUE))))
+
+      #--------------------------- Apply correction ---------------------------#
+      # Apply correction to all values of station 2
+      stn2$value <- stn2$value + bias
+
+      print("Correction applied")
+
+      #------------------------ Fill gaps in station 1 ------------------------#
+      # Where station 1 is missing data in range, fill in with corrected station 2
+      # Fill in all missing days in range for station 1 with NAs
+      all_dates <- seq(as.Date(start), as.Date(end), by = "days")
+      missing_dates <- as.Date(setdiff(all_dates, stn1$date))
+
+      if (length(missing_dates)>=1) {
+        missing_data <- data.frame(date = missing_dates, value = NA, variable = v)
+        stn1 <- dplyr::bind_rows(stn1[, c("date", "value", "variable")], missing_data)
+      }
+
+      # Replace NAs with corrected station 2
+      combined_data <- stn1 %>%
+        dplyr::left_join(stn2, by = "date", suffix = c("_stn1", "_stn2")) %>%
+        dplyr::mutate(value = dplyr::coalesce(.data$value_stn1, .data$value_stn2)
+                      , variable = .data$variable_stn1
+                      ) %>%
+        dplyr::select(date, value, variable) %>%
+        dplyr::arrange(date)
+
+      print("Gaps filled")
+
+      combined_variables <- rbind(combined_variables, combined_data)
+
+    }
+    combined_stations <- combined_variables
+  }
+return(combined_stations)
 }
+
 
