78e33c1406a19374dc5179f57bb44ba2e84afefa
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2024-02-29 05:24:19 +0000

    rename hydrometContinuous, make new plotting function. Clean up some app uneccessary files.

--- R/app_ui.R
+++ R/app_ui.R
@@ -21,8 +21,6 @@ app_ui <- function(request) {
     }'
 
   tagList(
-    # Leave this function for adding external resources
-    golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
       tags$head(
@@ -107,32 +105,5 @@ app_ui <- function(request) {
         )
       )
     )
-  )
-}
-
-
-#' Add external Resources to the Application
-#'
-#' This function is internally used to add external
-#' resources inside the Shiny application.
-#'
-#' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
-golem_add_external_resources <- function() {
-  add_resource_path(
-    "www",
-    app_sys("app/www")
-  )
-
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "hydroApp"
-    ),
-    # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert()
-    shinyjs::useShinyjs()
   )
 }
