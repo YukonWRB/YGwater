ff08a6cd7facb89af1c1226b3ff6bfaba1348002
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2024-04-18 20:26:04 +0000

    fix for VM operation.

--- R/streamLine.R
+++ R/streamLine.R
@@ -7,12 +7,14 @@
 #'
 #' @param host Host address. Leave default to run locally, set to "0.0.0.0" to enable others to connect. Depends on the port specified in `port` to be open on the host machine.
 #' @param port Port number (numeric). Leave default to use the default port specified in your user options. The port you specify must be open on the host machine for it to broadcast to the network.
+#' @param browser Open the application in a browser window right away (TRUE), or in a R window (FALSE). Default is TRUE.
+#' @param display.mode The display mode for the application. Default is "normal". See `shiny::runApp()` for more information.
 #' 
 #' @return Opens a Shiny application.
 #' @export
 #'
 
-streamLine <- function(host = getOption("shiny.host", "127.0.0.1"), port = getOption("shiny.port")) {
+streamLine <- function(host = getOption("shiny.host", "127.0.0.1"), port = getOption("shiny.port"), browser = TRUE, display.mode = "normal") {
   
   rlang::check_installed("shiny", reason = "required to use streamLine application.")
   rlang::check_installed("shinyjs", reason = "required to use streamLine application.")
@@ -30,5 +32,5 @@ streamLine <- function(host = getOption("shiny.host", "127.0.0.1"), port = getOp
   }
   
   shiny::enableBookmarking(store = "url")
-  shiny::runApp(appDir, display.mode = "normal", host = host, port = port, launch.browser = TRUE, quiet = FALSE)
+  shiny::runApp(appDir, display.mode = display.mode, host = host, port = port, launch.browser = browser, quiet = FALSE)
 }
