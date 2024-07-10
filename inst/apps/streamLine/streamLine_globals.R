3a0e8412f180392e9e2cfa55f20427dc085d8140
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2024-07-10 22:28:37 +0000

    Create option for apps to use dev database

---
+++ inst/apps/streamLine/streamLine_globals.R
@@ -0,0 +1,39 @@
+streamLine_globals <- function(dev = FALSE) {
+  library(shiny)
+  library(shinyjs)
+  
+  source(system.file("apps/streamLine/modules/mapView.R", package = "YGwater"))
+  source(system.file("apps/streamLine/modules/dataView.R", package = "YGwater"))
+  source(system.file("apps/streamLine/modules/homeView.R", package = "YGwater"))
+  source(system.file("apps/streamLine/modules/plotView.R", package = "YGwater"))
+  source(system.file("apps/streamLine/modules/docView.R", package = "YGwater"))
+  source(system.file("apps/streamLine/modules/imgView.R", package = "YGwater"))
+  source(system.file("apps/streamLine/modules/aboutView.R", package = "YGwater"))
+  
+  translations <<- data.table::setDT(openxlsx::read.xlsx(system.file("apps/streamLine/translations.xlsx", package = "YGwater"), sheet = 1))
+  
+  # Establish database connection
+  if (dev) {
+    if (!exists("pool")) {
+      pool <<- pool::dbPool(
+        drv = RPostgres::Postgres(),
+        dbname = "AquaCache",
+        host = Sys.getenv("AquaCacheHost"),
+        port = Sys.getenv("AquaCachePort"),
+        user = Sys.getenv("AquaCacheUser"),
+        password = Sys.getenv("AquaCachePass")
+      )
+    }
+  } else {
+    if (!exists("pool")) {
+      pool <<- pool::dbPool(
+        drv = RPostgres::Postgres(),
+        dbname = "hydromet",
+        host = Sys.getenv("hydrometHost"),
+        port = Sys.getenv("hydrometPort"),
+        user = Sys.getenv("hydrometUser"),
+        password = Sys.getenv("hydrometPass")
+      )
+    }
+  }
+}
