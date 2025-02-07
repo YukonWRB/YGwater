9c3d715d79adbaee7bb8081668d3c209ac9365a2
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-02-07 18:28:08 +0000

    .

--- R/globals.R
+++ R/globals.R
@@ -5,4 +5,5 @@ utils::globalVariables(c("timeseries"))
 utils::globalVariables(c("datetime_UTC", "value"))
 
 #globals for Shiny applications that source a script
-utils::globalVariables(c("floodAtlas_globals", "streamLine_globals", "YGwater_globals"))
+utils::globalVariables(c("floodAtlas_ts_globals", "floodAtlas_over_globals", "YGwater_globals"))
+
