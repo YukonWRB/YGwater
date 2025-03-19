1d6e4ee8bb867b050b20c74166e09b4ee40b6076
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-03-19 18:34:09 +0000

    Add Beta version message.

--- inst/apps/YGwater/modules/visualize/info/home.R
+++ inst/apps/YGwater/modules/visualize/info/home.R
@@ -2,10 +2,14 @@
 homeUI <- function(id) {
   ns <- NS(id)
   verticalLayout(
+    htmlOutput(ns("betaTitle")),
+    tags$div(style = "height: 10px;"),
+    htmlOutput(ns("betaText")),
+    tags$div(style = "height: 60px;"),
     htmlOutput(ns("title")),
     tags$div(style = "height: 10px;"),
     htmlOutput(ns("text")),
-    tags$div(style = "height: 60px;"),
+    tags$div(style = "height: 40px;"),
     htmlOutput(ns("discTitle")),
     tags$div(style = "height: 10px;"),
     htmlOutput(ns("discBody"))
@@ -19,6 +23,18 @@ home <- function(id, language) {
     
     observe({
       req(language$language)
+      output$betaTitle <- renderUI({
+        HTML(paste0('<div class="montserrat" style="font-size: 24px; font-weight: 600; font-style: normal">',
+                    tr("betaTitle", language$language),
+                    '</div>'
+        ))
+      })
+      output$betaText <- renderUI({
+        HTML(paste0('<div class="nunito-sans" style="font-size: 16px; font-weight: 500; font-style: normal;">',
+                    tr("betaText", language$language),
+                    '</div>'
+        ))
+      })
       output$title <- renderUI({
         HTML(paste0('<div class="montserrat" style="font-size: 24px; font-weight: 600; font-style: normal">',
                     tr("homeTitle", language$language),
