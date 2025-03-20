8856ac9cce740baddaf96806c105b8881d39e6e2
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-03-20 20:22:11 +0000

    snow template creation script now references circuits in the DB

--- R/createSnowTemplate.R
+++ R/createSnowTemplate.R
@@ -3,12 +3,14 @@
 #' @description
 #' `r lifecycle::badge("stable")`
 #'
+#' NOTE: locations must be associated with a circuit in the snow database for this function to work. If you are missing locations, you will need to add them to the snow database before running this function.
+#' 
 #' This function creates snow survey templates for a snow survey circuit in an excel workbook. The function takes the master template and copies it into multiple sheets with circuit name, course name and snow survey target date pre-filled. The outputted template is meant to be filled out by those conducting snow surveys.
 #' 
-#' You should be running this function from a computer with access to the Snow database for full functionality: 
+#' You should be running this function from a computer with access to the Snow database (and probably production instance) for full functionality.
 #'
 #' @param target_date The target date of the snow survey, given in the form yyyy-mm-dd as a text string or Date object. Example: for the march snow survey of 2024, target_date = '2024-03-01'
-#' @param circuit The circuit for which we are creating a snow survey template. Options are Carmacks, Dawson, HJ, KluaneP, Mayo, NorthSlope, OldCrow, PellyFarm, Ross, SLakes, Teslin, Watson, Whitehorse, and YEC. "all" will create a workbook for all circuits.
+#' @param circuits The circuit or circuits for which we are creating a snow survey template. Options are Alaska, Carmacks, Dawson, Haines Junction, Kluane National Park, Mayo, North Slope, Old Crow, Pelly Farm, Ross River, Southern Lakes, Teslin, Watson Lake, Whitehorse, YEC. "all" will create a workbook for all circuits listed in the Snow database. Default is "all".
 #' @param save_path The path where you want the circuit workbook(s) saved. Default "choose" lets you pick your folder.
 #' @param snowCon A snow database connection object. If left NULL the function will attempt to connect to the snow database using function [snowConnect()] and close the connection when finished.
 #' 
@@ -17,7 +19,7 @@
 #' @export
 #'
 
-createSnowTemplate <- function(target_date, circuit = "all", save_path = "choose", snowCon = NULL) {
+createSnowTemplate <- function(target_date, circuits = "all", save_path = "choose", snowCon = NULL) {
   
   if (save_path == "choose") {
     if (!interactive()) {
@@ -37,89 +39,37 @@ createSnowTemplate <- function(target_date, circuit = "all", save_path = "choose
     stop("target_date must be a character string or Date object")
   }
   
-  if (length(circuit) != 1) {
-    stop("parameter circuit must be a character vector of length 1, either 'all' or a specific circuit name.")
+  if (is.null(snowCon)) {
+    snowCon <- snowConnect(silent = TRUE)
+    on.exit(DBI::dbDisconnect(snowCon), add = TRUE)
   }
   
-  circuit <- tolower(circuit)
-  
-  if (circuit == "all") {
-    circuits <- c("carmacks", "dawson", "hj", "kluanep", "mayo", "northslope", "oldcrow", "pellyfarm", "ross", "slakes", "teslin", "watson", "whitehorse", "yec")
+  if (circuits[1] == "all") {
+    circuits <- DBI::dbGetQuery(snowCon, "SELECT DISTINCT * FROM circuits")
   } else {
-    circuits <- circuit
+    # Make sure that the requested circuit exist in the database
+    circuits <- DBI::dbGetQuery(snowCon, paste0("SELECT * FROM circuits WHERE circuit_name IN ('", paste(circuits, collapse = "', '"), "')"))
+    if (nrow(circuits) != length(circuits)) {
+      valid_circuits <- DBI::dbGetQuery(snowCon, "SELECT circuit_name FROM circuits")
+      stop("One or more circuits not found in snow database. Please enter a valid circuit name. Valid names are ", paste(valid_circuits$circuit_name, collapse = ", "))
+    }
   }
   
-  for (circuit in circuits) {
+  for (i in 1:nrow(circuits)) {
+    
+    circuit <- circuits$circuit_name[i]
+    circuit_id <- circuits$circuit_id[i]
     
     template <- openxlsx::loadWorkbook(system.file("snow_survey/SnowSurveyTemplate.xlsx", package = "YGwater")) # reloaded each time to start from original
     
     
     #### ----------------------- Create circuits ------------------------------ ####
-    if (circuit == "carmacks") {
-      courses <- c("Casino Creek", "MacIntosh", "Mount Berdoe", "Mount Nansen", "Satasha Lake", "Williams Creek")
-    } else if (circuit == "dawson") {
-      courses <- c("Blackstone River", "Eagle Plains", "Eagle River", "Grizzly Creek", "King Solomon Dome", "King Solomon Dome Scale", "Midnight Dome", "Ogilvie River", "Riffs Ridge")
-    } else if (circuit == "hj") {
-      courses <- c("Beaver Creek", "Burwash Airstrip", "Chair Mountain", "Haines Junction Farm", "Summit")
-    } else if (circuit == "kluanep") {
-      courses <- c("Alder Creek")
-    } else if (circuit == "mayo") {
-      courses <- c("Calumet", "Mayo Airport A", "Mayo Airport B", "Mayo Airport C") #
-    } else if (circuit == "northslope") {
-      courses <- c("AK Border", "Herschel Island", "Komakuk Beach", "NWT/YK Border", "Stokes Point", "Shingle Point")
-    } else if (circuit == "oldcrow") {
-      courses <- c("Old Crow")
-    } else if (circuit == "pellyfarm") {
-      courses <- c("Pelly Farm")
-    } else if (circuit == "ross") { # Snow scale/pillow locations not in  snow db. Twin Creeks A not in aquacache, but is in snow db
-      courses <- c("Bonnet Plume Lake", "Burns Lake", "Edwards Lake", "Finlayson Airstrip", "Ford Lake", "Fuller Lake", "Hoole River", "Jordan Lake", "Plata Airstrip", "Rackla Lake", "Rose Creek", "Russell Lake", "Tintina Airstrip", "Twin Creeks A", "Twin Creeks B", "Withers Lake", "Twin Creeks B Snow Scale", "Withers Pillow", "Withers Scale") #
-    } else if (circuit == "slakes") { # Snow scale/pillow locations not in db.
-      courses <- c("Atlin (B.C.)", "Log Cabin (B.C.)", "Log Cabin Pillow (B.C.)", "Montana Mountain", "Montana Mountain Pillow", "Tagish", "Tagish Snow Scale", "Tagish Snow Pillow")
-    } else if (circuit == "teslin") {
-      courses <- c("Meadow Creek", "Morley Lake", "Pine Lake Airstrip")
-    } else if (circuit == "watson") {
-      courses <- c("Frances River", "Hyland River B", "Hyland Snow Scale", "Watson Lake Airport") # Hyland River does not exist in aquacache db, but is in snow db
-    } else if (circuit == "whitehorse") { # Buckbrush snow scales is not in snow db. Whitehorse Airport is actually Whitehorse Airport A
-      courses <- c("Buckbrush Snow Pillow", "Mt McIntyre B", "Whitehorse Airport", "Whitehorse Airport B_Ice Lake")
-    } else if (circuit == "yec") {
-      courses <- c("Aishihik Lake", "Canyon Lake")
-    } else {
-      stop("Circuit not recognized. Please enter a valid circuit name.")
-    }
+    # Get the associated locations for the circuit
+    courses <- DBI::dbGetQuery(snowCon, paste0("SELECT name FROM locations WHERE circuit = '", circuit_id, "'"))[,1]
     
     ## Get maintenance for all courses
-      if (is.null(snowCon)) { # Create new connection
-        tryCatch({
-          snowCon <- snowConnect(silent = TRUE)
-          on.exit(DBI::dbDisconnect(snowCon), add = TRUE)
-          maintenance <- DBI::dbGetQuery(snowCon, paste0("SELECT maintenance.maintenance, locations.location, locations.name FROM maintenance ",
-                                                     "INNER JOIN locations ON maintenance.location = locations.location " ,
-                                                     "WHERE completed = FALSE AND name IN ('", paste(courses, collapse = "', '"), "') ",
-                                                     "AND completed = FALSE"))
-          snowCon_flag <- TRUE
-        }, error = function(e) {
-          snowCon_flag <<- FALSE
-          warning("Could not connect to snow database. Maintenance information will not be included in the report. Do you have the correct credentials in your .Renviron file?")
-        })
-      } else { # Use existing connection (don't close it afterwards)
-        # Check that the existing connection is valid
-        check <- DBI::dbGetQuery(snowCon, "SELECT 1")
-        if (nrow(check) == 0) {
-          snowCon_flag <<- FALSE
-          warning("Existing snow database connection is invalid. Maintenance information will not be included in the report.")
-        } else {
-          tryCatch({
-            maintenance <- DBI::dbGetQuery(snowCon, paste0("SELECT maintenance.maintenance, locations.location, locations.name FROM maintenance ",
-                                                           "INNER JOIN locations ON maintenance.location = locations.location " ,
-                                                           "WHERE completed = FALSE AND name IN ('", paste(courses, collapse = "', '"), "') ",
-                                                           "AND completed = FALSE"))
-            snowCon_flag <- TRUE
-          }, error = function(e) {
-            snowCon_flag <<- FALSE
-            warning("Could not connect to snow database with the connection you provided. Maintenance information will not be included in the report.")
-          }) # End tryCatch
-        }
-      }
+    maintenance <- DBI::dbGetQuery(snowCon, paste0("SELECT maintenance.maintenance, locations.location, locations.name FROM maintenance INNER JOIN locations ON maintenance.location = locations.location WHERE maintenance.completed = FALSE AND locations.name IN ('", paste(courses, collapse = "', '"), "') AND maintenance.completed = FALSE"))
+    
     
     #### ------------ Add sheet for each snow course of the circuit ----------- ####
     # Put courses in alphabetical order
@@ -142,14 +92,13 @@ createSnowTemplate <- function(target_date, circuit = "all", save_path = "choose
       openxlsx::writeData(template, sheet = sheet_names[c], x = circuit, xy = c(4,4))
       openxlsx::writeData(template, sheet = sheet_names[c], x = courses[c], xy = c(4,5))
       openxlsx::writeData(template, sheet = sheet_names[c], x = target_date, xy = c(4,6))
+      
       # Fill in maintenance
-      if (snowCon_flag) {
-        maint <- maintenance[maintenance$name == courses[c],]
-        for (m in maint$maintenance) {
-          if (m == "Brush snow course") {openxlsx::writeData(template, sheet = sheet_names[c], x = "x", xy = c(9,49))}
-          if (m == "Brush helipad/access trail") {openxlsx::writeData(template, sheet = sheet_names[c], x = "x", xy = c(9,50))}
-          if (m == "Replace marker plate/plates") {openxlsx::writeData(template, sheet = sheet_names[c], x = "x", xy = c(9,51))}
-        }
+      maint <- maintenance[maintenance$name == courses[c],]
+      for (m in maint$maintenance) {
+        if (m == "Brush snow course") {openxlsx::writeData(template, sheet = sheet_names[c], x = "x", xy = c(9,49))}
+        if (m == "Brush helipad/access trail") {openxlsx::writeData(template, sheet = sheet_names[c], x = "x", xy = c(9,50))}
+        if (m == "Replace marker plate/plates") {openxlsx::writeData(template, sheet = sheet_names[c], x = "x", xy = c(9,51))}
       }
     }
     
@@ -161,29 +110,26 @@ createSnowTemplate <- function(target_date, circuit = "all", save_path = "choose
     
     #### ------------------------- Create summary sheet ----------------------- ####
     # Pull data from snow database
-    if (snowCon_flag) {
-      summary <- SWE_station(year = as.numeric(substr(target_date, start = 1, stop = 4)),
-                             month = as.numeric(substr(target_date, start = 7, stop = 7)),
-                             return_missing = TRUE, 
-                             source = "snow",
-                             snowCon = snowCon)
-      # Subset to locations of interest and columns of interest
-      summary <- summary[summary$location_name %in% courses, c("location_name", "location_id", "swe_prevyear", "swe_med")]
-      # Add locations to summary that are not in database
-      # Get courses that are not in summary, but in courses
-      locs <- setdiff(courses, summary$location_name)
-      summary <- rbind(summary, data.frame("location_name" = locs, "location_id" = rep(NA, times = length(locs)), "swe_prevyear" = rep(NA, times = length(locs)), "swe_med" = rep(NA, times = length(locs))))
-      
-      # Put into alphabetical order
-      summary <- summary[order(summary$location_name), ]
-      
-      # Add data to summary worksheet
-      openxlsx::writeData(template, sheet = "Summary", x = summary[1:2], xy = c(1,3), colNames = FALSE)
-      openxlsx::writeData(template, sheet = "Summary", x = summary[3:4], xy = c(7,3), colNames = FALSE)
-    } else {
-      openxlsx::writeData(template, sheet = "Summary", x = "No DB connection", xy = c(1,3), colNames = FALSE)
-      openxlsx::writeData(template, sheet = "Summary", x = "No DB connection", xy = c(7,3), colNames = FALSE)
-    }
+    
+    summary <- SWE_station(year = as.numeric(substr(target_date, start = 1, stop = 4)),
+                           month = as.numeric(substr(target_date, start = 7, stop = 7)),
+                           return_missing = TRUE, 
+                           source = "snow",
+                           snowCon = snowCon)
+    # Subset to locations of interest and columns of interest
+    summary <- summary[summary$location_name %in% courses, c("location_name", "location_id", "swe_prevyear", "swe_med")]
+    # Add locations to summary that are not in database
+    # Get courses that are not in summary, but in courses
+    locs <- setdiff(courses, summary$location_name)
+    ids <- DBI::dbGetQuery(snowCon, paste0("SELECT location FROM locations WHERE name IN ('", paste(locs, collapse = "', '"), "')"))[,1]
+    summary <- rbind(summary, data.frame("location_name" = locs, "location_id" = ids, "swe_prevyear" = rep(NA, times = length(locs)), "swe_med" = rep(NA, times = length(locs))))
+    
+    # Put into alphabetical order
+    summary <- summary[order(summary$location_name), ]
+    
+    # Add data to summary worksheet
+    openxlsx::writeData(template, sheet = "Summary", x = summary[1:2], xy = c(1,3), colNames = FALSE)
+    openxlsx::writeData(template, sheet = "Summary", x = summary[3:4], xy = c(7,3), colNames = FALSE)
     
     # Add formula to get values from other sheets
     # Sample date
@@ -236,4 +182,5 @@ createSnowTemplate <- function(target_date, circuit = "all", save_path = "choose
                            file = paste0(save_path, "/", circuit, "_", target_date, ".xlsx"),
                            overwrite = TRUE)
   }
+  message("Snow survey template(s) created and saved.")
 }
