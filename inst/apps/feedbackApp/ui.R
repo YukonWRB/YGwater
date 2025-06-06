c5c9b4ccefc2c5be26db6d3a6ca0bd557471fd83
Ghislain de Laplante <91148465+gdelaplante@users.noreply.github.com>
2025-06-05 22:20:07 -0700

    Update ui.R

--- inst/apps/feedbackApp/ui.R
+++ inst/apps/feedbackApp/ui.R
@@ -8,13 +8,14 @@
 app_ui <- function(request) {
 
   fluidPage(
-    titlePanel("YGwater Feedback"),
+    titlePanel("Water Data Explorer Feedback"),
     textInput("user", "Name or Email (optional)"),
-    textAreaInput("bugs", "Did you encounter any bugs? If yes, please describe:", "", rows = 3),
-    textAreaInput("ui_comment", "Comments about the user interface:", "", rows = 3),
-    textAreaInput("module_comment", "Comments about any particular module:", "", rows = 3),
+    textAreaInput("bugs", "Did you encounter any bugs? If yes, please describe what the bug was and how it was triggered.", "", rows = 3),
+    textAreaInput("ui_comment", "Do you have any comments or concerns about the user interface in general?", "", rows = 3),
+    textAreaInput("module_comment", "Do you have comments about any particular module (page)?", "", rows = 3),
     radioButtons("got_data", "Did the app give you the data or visualization you were seeking?", choices = c("Yes", "No")),
-    textAreaInput("missing", "If not, what was missing?", "", rows = 3),
+    textAreaInput("missing", "What was missing?", "", rows = 3),
+    textAreInput("addtional_comments", "Do you have any additional comments?", "", rows = 3),
     actionButton("submit", "Submit Feedback"),
     textOutput("thanks")
   )
