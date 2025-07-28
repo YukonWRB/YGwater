e28cf2969ef734ecf014280f6bad874eb2285ef3
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-07-28 21:17:06 +0000

    .

--- tests/testthat/test-plotTimeseries.R
+++ tests/testthat/test-plotTimeseries.R
@@ -3,37 +3,37 @@
 test_that("timeseries plot is as expected for one year with no historic range or slider", {
   skip_on_cran()
   plot <- plotTimeseries(location = "09EA004", parameter = "water level", start_date = "2016-01-01", end_date = "2017-01-01", historic_range = FALSE, slider = FALSE)
-  vdiffr::expect_doppelganger("timeseries_plot_no_historic_range_no_slider", plot)
+  vdiffr::expect_doppelganger("plot_no_hist_range_no_slider", print(plot))
 })
 
 test_that("timeseries plot is as expected for one year with no historic range", {
   skip_on_cran()
   plot <- plotTimeseries(location = "09EA004", parameter = "water level", start_date = "2016-01-01", end_date = "2017-01-01", historic_range = FALSE)
-  vdiffr::expect_doppelganger("timeseries_plot_no_historic_range", plot)
+  vdiffr::expect_doppelganger("plot_no_hist_range", plot)
 })
 
 test_that("timeseries plot is as expected for one year with historic range", {
   skip_on_cran()
   plot <- plotTimeseries(location = "09EA004", parameter = "water level", start_date = "2016-01-01", end_date = "2017-01-01")
-  vdiffr::expect_doppelganger("timeseries_plot_with_historic_range", plot)
+  vdiffr::expect_doppelganger("plot_w_hist_range", plot)
 })
 
 test_that("French timeseries plot is as expected for one year with historic range and slider", {
   skip_on_cran()
   plot <- plotTimeseries(location = "09EA004", parameter = "water level", start_date = "2016-01-01", end_date = "2017-01-01", lang = "fr")
-  vdiffr::expect_doppelganger("french_timeseries_plot_with_historic_range_and_slider", plot)
+  vdiffr::expect_doppelganger("fr_plot_w_hist_rnge_and_slider", plot)
 })
 
 test_that("French timeseries plot is as expected for one year with historic range and no slider", {
   skip_on_cran()
   plot <- plotTimeseries(location = "09EA004", parameter = "water level", start_date = "2016-01-01", end_date = "2017-01-01", lang = "fr", slider = FALSE)
-  vdiffr::expect_doppelganger("french_timeseries_plot_with_historic_range_no_slider", plot)
+  vdiffr::expect_doppelganger("fr_plot_w_hist_rng_no_slider", plot)
 })
 
 test_that("grades, approvals, qualifiers are displayed", {
   skip_on_cran()
   plot <- plotTimeseries(location = "09EA004", parameter = "water level", start_date = "2016-01-01", end_date = "2017-01-01", lang = "fr", slider = FALSE, grades = TRUE, qualifiers = TRUE, approvals = TRUE)
-  vdiffr::expect_doppelganger("timeseries_plot_with_grades_approvals_qualifiers", plot)
+  vdiffr::expect_doppelganger("plot_w_grades_approvals_qualifiers", plot)
 })
 
 test_that("one of grades, approvals, qualifiers is displayed", {
