ec5772ff43824722311b7228149dec1478f4f3d0
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-07-29 21:45:26 +0000

    .

--- R/getSpatial.R
+++ R/getSpatial.R
@@ -2,189 +2,194 @@
 #'
 #' @description
 #' `r lifecycle::badge('experimental')`
-#' 
-#' Placeholder function, not yet complete.
 #'
-#' @param clauses Character SQL clauses to filter the raster data and return the results. Must begin with 'WHERE' and be formatted as a valid SQL WHERE clause. For example, 'WHERE reference_id = 1. If NULL, all raster data is returned.
+#' @param clauses Character SQL clauses to filter the raster data and return the results. Must begin with 'WHERE' and be formatted as a valid SQL WHERE clause. For example, 'WHERE reference_id = 1'. If NULL, all raster data is returned.
 #' @param boundary Optional spatial boundary to limit the extent of the raster data returned, as a terra::spatVector object or a numeric vector c(top, bottom, right, left) indicating the projection-specific limits with which to clip the raste (by default, decimal degree lat/long).  If NULL, the full extent of the raster data is returned.
 #' @param bands The raster bands to return. Default is 1, which returns the first band of the raster data. If you want to return all bands, set this to TRUE.
-#' @param tbl_name The name of the schema and table containing the raster data. Default is "spatial.rasters".
+#' @param tbl_name The name of the schema and table containing the raster data. Default is c("spatial", "rasters").
 #' @param col_name The name of the column containing the raster data. Default is "rast".
 #' @param con A connection to the target database. If NULL, a new connection is created using [AquaConnect()] and automatically closed when the function exits.
 #' @return A terra::rast object containing the raster data.
 #' @export
 #'
 
-
-
-# Generated from function body. Editing this file has no effect.
-getRaster <- function (conn, name = c("spatial", "rasters"), rast = "rast", bands = 1, boundary = NULL, 
-    clauses = NULL, progress = TRUE) 
+getRaster <- function(clauses = NULL,
+                      boundary = NULL,
+                      tbl_name = c("spatial", "rasters"), 
+                      col_name = "rast", 
+                      bands = 1,
+                      con = NULL) 
 {
-    #warn_deprecated_rc(returnclass, "pgGetRast(returnclass = 'should be a `terra` object')")
-    rpostgis::dbConnCheck(conn)
-    if (!suppressMessages(rpostgis::pgPostGIS(conn))) {
-        cli::cli_abort("PostGIS is not enabled on this database.")
-    }
-    name1 <- rpostgis::dbTableNameFix(conn, name)
-    nameque <- paste(name1, collapse = ".")
-    namechar <- gsub("'", "''", paste(gsub("^\"|\"$", "", name1), 
-        collapse = "."))
-    rastque <- DBI::dbQuoteIdentifier(conn, rast)
-    clauses2 <- sub("^where", "AND", clauses, ignore.case = TRUE)
-    tmp.query <- paste0("SELECT r_raster_column AS geo FROM raster_columns\n  WHERE (r_table_schema||'.'||r_table_name) = '", 
-        namechar, "';")
-    tab.list <- DBI::dbGetQuery(conn, tmp.query)$geo
-    if (is.null(tab.list)) {
-        cli::cli_abort("Table '{namechar}' is not listed in raster_columns.")
-    }
-    else if (!rast %in% tab.list) {
-        cli::cli_abort("Table '{namechar}' raster column '{rast}' not found. Available raster columns: {paste(tab.list, collapse = ', ')}")
-    }
-    tmp.query <- paste0("SELECT st_numbands(", rastque, ") FROM ", 
-        nameque, " WHERE ", rastque, " IS NOT NULL LIMIT 1;")
-    nbs <- 1:DBI::dbGetQuery(conn, tmp.query)[1, 1]
-    if (isTRUE(bands)) {
-        bands <- nbs
-    }
-    else if (!all(bands %in% nbs)) {
-        cli::cli_abort("Selected band(s) do not exist in PostGIS raster: choose band numbers between {min(nbs)} and {max(nbs)}.")
-          }
-          # Add WHERE clause to restrict to non-null rasters
-    tmp.query <- paste0("SELECT DISTINCT(ST_SRID(", rastque, 
-      ")) FROM ", nameque, " WHERE ", rastque, " IS NOT NULL", if (!is.null(clauses2)) paste0(" ", clauses2) else "", ";")
-
-    srid <- DBI::dbGetQuery(conn, tmp.query)
-
-
-    #if (nrow(srid) > 1) {
-    #    cli::cli_abort("Multiple SRIDs in the raster")
-    #}
-    #else if (nrow(srid) < 1) {
-    #    cli::cli_abort("Database table is empty.")
-    #}
-
-
-    p4s <- NA
-    tmp.query.sr <- paste0("SELECT r_proj4 AS p4s FROM ", nameque, 
-        ";")
-    try(db.proj4 <- DBI::dbGetQuery(conn, tmp.query.sr)$p4s, silent = TRUE)
-    if (!exists("db.proj4")) {
-        tmp.query.sr <- paste0("SELECT proj4text AS p4s FROM spatial_ref_sys WHERE srid = ", 
-            srid$st_srid, ";")
-        db.proj4 <- DBI::dbGetQuery(conn, tmp.query.sr)$p4s
-    }
-    if (!is.null(db.proj4)) {
-        try(p4s <- terra::crs(db.proj4[1]), silent = TRUE)
-    }
-    if (is.na(p4s)) {
-        cli::cli_alert_warning("Table SRID not found. Projection will be undefined (NA)")
-    }
-    tmp.query <- paste0("SELECT ST_SameAlignment(", rastque, 
-        ") FROM ", nameque, ";")
-    al <- FALSE
-    try(al <- DBI::dbGetQuery(conn, tmp.query)[1, 1])
-    if (!al) {
-        tmp.query <- paste0("SELECT min(ST_UpperLeftX(", rastque, 
-            ")) ux, max(ST_UpperLeftY(", rastque, ")) uy FROM ", 
-            nameque, ";")
-        aligner <- DBI::dbGetQuery(conn, tmp.query)
-        aq <- c("ST_SnapToGrid(", paste0(aligner[1, 1], ","), 
+  
+  if (is.null(con)) {
+    con <- AquaConnect(silent = TRUE)
+    on.exit(DBI::dbDisconnect(con))
+  }
+  
+  #warn_deprecated_rc(returnclass, "pgGetRast(returnclass = 'should be a `terra` object')")
+  rpostgis:::dbConnCheck(con)
+  if (!suppressMessages(rpostgis::pgPostGIS(con))) {
+    cli::cli_abort("PostGIS is not enabled on this database.")
+  }
+  name1 <- rpostgis:::dbTableNameFix(con, tbl_name)
+  nameque <- paste(name1, collapse = ".")
+  namechar <- gsub("'", "''", paste(gsub("^\"|\"$", "", name1), 
+                                    collapse = "."))
+  rastque <- DBI::dbQuoteIdentifier(con, col_name)
+  clauses2 <- sub("^where", "AND", clauses, ignore.case = TRUE)
+  tmp.query <- paste0("SELECT r_raster_column AS geo FROM raster_columns\n  WHERE (r_table_schema||'.'||r_table_name) = '", 
+                      namechar, "';")
+  tab.list <- DBI::dbGetQuery(con, tmp.query)$geo
+  if (is.null(tab.list)) {
+    cli::cli_abort("Table '{namechar}' is not listed in raster_columns.")
+  }
+  else if (!col_name %in% tab.list) {
+    cli::cli_abort("Table '{namechar}' raster column '{col_name}' not found. Available raster columns: {paste(tab.list, collapse = ', ')}")
+  }
+  tmp.query <- paste0("SELECT st_numbands(", rastque, ") FROM ", 
+                      nameque, " WHERE ", rastque, " IS NOT NULL LIMIT 1;")
+  nbs <- 1:DBI::dbGetQuery(con, tmp.query)[1, 1]
+  if (isTRUE(bands)) {
+    bands <- nbs
+  }
+  else if (!all(bands %in% nbs)) {
+    cli::cli_abort("Selected band(s) do not exist in PostGIS raster: choose band numbers between {min(nbs)} and {max(nbs)}.")
+  }
+  # Add WHERE clause to restrict to non-null rasters
+  tmp.query <- paste0("SELECT DISTINCT(ST_SRID(", rastque, 
+                      ")) FROM ", nameque, " WHERE ", rastque, " IS NOT NULL", if (!is.null(clauses2)) paste0(" ", clauses2) else "", ";")
+  
+  srid <- DBI::dbGetQuery(con, tmp.query)
+  
+  
+  #if (nrow(srid) > 1) {
+  #    cli::cli_abort("Multiple SRIDs in the raster")
+  #}
+  #else if (nrow(srid) < 1) {
+  #    cli::cli_abort("Database table is empty.")
+  #}
+  
+  
+  p4s <- NA
+  tmp.query.sr <- paste0("SELECT r_proj4 AS p4s FROM ", nameque, 
+                         ";")
+  try(db.proj4 <- DBI::dbGetQuery(con, tmp.query.sr)$p4s, silent = TRUE)
+  if (!exists("db.proj4")) {
+    tmp.query.sr <- paste0("SELECT proj4text AS p4s FROM spatial_ref_sys WHERE srid = ", 
+                           srid$st_srid, ";")
+    db.proj4 <- DBI::dbGetQuery(con, tmp.query.sr)$p4s
+  }
+  if (!is.null(db.proj4)) {
+    try(p4s <- terra::crs(db.proj4[1]), silent = TRUE)
+  }
+  if (is.na(p4s)) {
+    cli::cli_alert_warning("Table SRID not found. Projection will be undefined (NA)")
+  }
+  tmp.query <- paste0("SELECT ST_SameAlignment(", rastque, 
+                      ") FROM ", nameque, ";")
+  al <- FALSE
+  try(al <- DBI::dbGetQuery(con, tmp.query)[1, 1])
+  if (!al) {
+    tmp.query <- paste0("SELECT min(ST_UpperLeftX(", rastque, 
+                        ")) ux, max(ST_UpperLeftY(", rastque, ")) uy FROM ", 
+                        nameque, ";")
+    aligner <- DBI::dbGetQuery(con, tmp.query)
+    aq <- c("ST_SnapToGrid(", paste0(aligner[1, 1], ","), 
             paste0(aligner[1, 2], "),"))
+  }
+  else {
+    aq <- NULL
+  }
+  get_band <- function(band) {
+    info <- DBI::dbGetQuery(con, paste0("select\n            st_xmax(st_envelope(rast)) as xmax,\n            st_xmin(st_envelope(rast)) as xmin,\n            st_ymax(st_envelope(rast)) as ymax,\n            st_ymin(st_envelope(rast)) as ymin,\n            st_width(rast) as cols,\n            st_height(rast) as rows\n            from\n            (select st_union(", 
+                                         aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
+                                         nameque, " ", clauses, ") as a;"))
+    vals <- DBI::dbGetQuery(con, paste0("select\n          unnest(st_dumpvalues(rast, 1)) as vals\n          from\n          (select st_union(", 
+                                         aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
+                                         nameque, " ", clauses, ") as a;"))$vals
+    rout <- terra::rast(nrows = info$rows, ncols = info$cols, 
+                        xmin = info$xmin, xmax = info$xmax, ymin = info$ymin, 
+                        ymax = info$ymax, crs = p4s, vals = vals)
+    return(rout)
+  }
+  get_band_boundary <- function(band) {
+    info <- DBI::dbGetQuery(con, paste0("select\n            st_xmax(st_envelope(rast)) as xmx,\n            st_xmin(st_envelope(rast)) as xmn,\n            st_ymax(st_envelope(rast)) as ymx,\n            st_ymin(st_envelope(rast)) as ymn,\n            st_width(rast) as cols,\n            st_height(rast) as rows\n            from\n            (select st_union(", 
+                                         aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
+                                         nameque, "\n\n            WHERE ST_Intersects(", 
+                                         rastque, ",ST_SetSRID(ST_GeomFromText('POLYGON((", 
+                                         boundary[4], " ", boundary[1], ",", boundary[4], 
+                                         " ", boundary[2], ",\n  ", boundary[3], " ", boundary[2], 
+                                         ",", boundary[3], " ", boundary[1], ",", boundary[4], 
+                                         " ", boundary[1], "))'),", srid, "))", clauses2, 
+                                         ") as a;"))
+    if (is.na(info$cols) & is.na(info$rows)) {
+      stop("No data found within geographic subset defined by 'boundary'.")
     }
-    else {
-        aq <- NULL
+    vals <- DBI::dbGetQuery(con, paste0("select\n          unnest(st_dumpvalues(rast, 1)) as vals\n          from\n          (select st_union(", 
+                                         aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
+                                         nameque, "\n\n            WHERE ST_Intersects(", 
+                                         rastque, ",ST_SetSRID(ST_GeomFromText('POLYGON((", 
+                                         boundary[4], " ", boundary[1], ",", boundary[4], 
+                                         " ", boundary[2], ",\n  ", boundary[3], " ", boundary[2], 
+                                         ",", boundary[3], " ", boundary[1], ",", boundary[4], 
+                                         " ", boundary[1], "))'),", srid, "))", clauses2, 
+                                         ") as a;"))$vals
+    rout <- terra::rast(nrows = info$rows, ncols = info$cols, 
+                        xmin = info$xmn, xmax = info$xmx, ymin = info$ymn, 
+                        ymax = info$ymx, crs = p4s, vals = vals)
+    return(rout)
+  }
+  
+  
+  
+  cli::cli_alert_info("Reading {length(bands)} band{?s}...")
+  download_pb <- cli::cli_progress_bar("Read bands", total = length(bands), 
+                                       type = "tasks", format_done = "{.alert-success Read completed {.timestamp {cli::pb_elapsed}}}", 
+                                       clear = FALSE)
+  rout <- list()
+  if (is.null(boundary)) {
+    for (i in seq_along(bands)) {
+      rout[[i]] <- get_band(bands[i])
+      cli::cli_progress_update(id = download_pb)
     }
-    get_band <- function(band) {
-        info <- DBI::dbGetQuery(conn, paste0("select\n            st_xmax(st_envelope(rast)) as xmax,\n            st_xmin(st_envelope(rast)) as xmin,\n            st_ymax(st_envelope(rast)) as ymax,\n            st_ymin(st_envelope(rast)) as ymin,\n            st_width(rast) as cols,\n            st_height(rast) as rows\n            from\n            (select st_union(", 
-            aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
-            nameque, " ", clauses, ") as a;"))
-        vals <- DBI::dbGetQuery(conn, paste0("select\n          unnest(st_dumpvalues(rast, 1)) as vals\n          from\n          (select st_union(", 
-            aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
-            nameque, " ", clauses, ") as a;"))$vals
-        rout <- terra::rast(nrows = info$rows, ncols = info$cols, 
-            xmin = info$xmin, xmax = info$xmax, ymin = info$ymin, 
-            ymax = info$ymax, crs = p4s, vals = vals)
-        return(rout)
+    rb <- terra::rast(rout)
+  }
+  else {
+    if (inherits(boundary, "sf")) {
+      boundary <- sf::st_bbox(boundary)
+      boundary <- c(boundary[4], boundary[2], boundary[3], 
+                    boundary[1])
     }
-    get_band_boundary <- function(band) {
-        info <- DBI::dbGetQuery(conn, paste0("select\n            st_xmax(st_envelope(rast)) as xmx,\n            st_xmin(st_envelope(rast)) as xmn,\n            st_ymax(st_envelope(rast)) as ymx,\n            st_ymin(st_envelope(rast)) as ymn,\n            st_width(rast) as cols,\n            st_height(rast) as rows\n            from\n            (select st_union(", 
-            aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
-            nameque, "\n\n            WHERE ST_Intersects(", 
-            rastque, ",ST_SetSRID(ST_GeomFromText('POLYGON((", 
-            boundary[4], " ", boundary[1], ",", boundary[4], 
-            " ", boundary[2], ",\n  ", boundary[3], " ", boundary[2], 
-            ",", boundary[3], " ", boundary[1], ",", boundary[4], 
-            " ", boundary[1], "))'),", srid, "))", clauses2, 
-            ") as a;"))
-        if (is.na(info$cols) & is.na(info$rows)) {
-            stop("No data found within geographic subset defined by 'boundary'.")
-        }
-        vals <- DBI::dbGetQuery(conn, paste0("select\n          unnest(st_dumpvalues(rast, 1)) as vals\n          from\n          (select st_union(", 
-            aq[1], rastque, ",", aq[2], aq[3], band, ") rast from ", 
-            nameque, "\n\n            WHERE ST_Intersects(", 
-            rastque, ",ST_SetSRID(ST_GeomFromText('POLYGON((", 
-            boundary[4], " ", boundary[1], ",", boundary[4], 
-            " ", boundary[2], ",\n  ", boundary[3], " ", boundary[2], 
-            ",", boundary[3], " ", boundary[1], ",", boundary[4], 
-            " ", boundary[1], "))'),", srid, "))", clauses2, 
-            ") as a;"))$vals
-        rout <- terra::rast(nrows = info$rows, ncols = info$cols, 
-            xmin = info$xmn, xmax = info$xmx, ymin = info$ymn, 
-            ymax = info$ymx, crs = p4s, vals = vals)
-        return(rout)
+    else if (inherits(boundary, "SpatVector")) {
+      boundary <- c(terra::ext(boundary)[4], terra::ext(boundary)[3], 
+                    terra::ext(boundary)[2], terra::ext(boundary)[1])
     }
-
-
-    
-    cli::cli_alert_info("Reading {length(bands)} band{?s}...")
-    download_pb <- cli::cli_progress_bar("Read bands", total = length(bands), 
-        type = "tasks", format_done = "{.alert-success Read completed {.timestamp {cli::pb_elapsed}}}", 
-        clear = FALSE)
-    rout <- list()
-    if (is.null(boundary)) {
-        for (i in seq_along(bands)) {
-            rout[[i]] <- get_band(bands[i])
-            cli::cli_progress_update(id = download_pb)
-        }
-        rb <- terra::rast(rout)
+    extclip <- terra::ext(boundary[4], boundary[3], boundary[2], 
+                          boundary[1])
+    for (i in seq_along(bands)) {
+      rout[[i]] <- get_band_boundary(bands[i])
+      cli::cli_progress_update(id = download_pb)
     }
-    else {
-        if (inherits(boundary, "sf")) {
-            boundary <- sf::st_bbox(boundary)
-            boundary <- c(boundary[4], boundary[2], boundary[3], 
-                boundary[1])
-        }
-        else if (inherits(boundary, "SpatVector")) {
-            boundary <- c(terra::ext(boundary)[4], terra::ext(boundary)[3], 
-                terra::ext(boundary)[2], terra::ext(boundary)[1])
-        }
-        extclip <- terra::ext(boundary[4], boundary[3], boundary[2], 
-            boundary[1])
-        for (i in seq_along(bands)) {
-            rout[[i]] <- get_band_boundary(bands[i])
-            cli::cli_progress_update(id = download_pb)
-        }
-        rb <- terra::rast(rout)
-    }
-
-    cli::cli_process_done(id = download_pb)
-
-    if ("band_names" %in% rpostgis:::dbTableInfo(conn, name)$column_name) {
-        try({
-            ct <- 1
-            for (b in bands) {
-                lnm <- DBI::dbGetQuery(conn, paste0("SELECT DISTINCT band_names[", 
-                  b, "][1] as nm FROM ", nameque, " ", clauses, 
-                  ";"))
-                names(rb)[ct] <- lnm$nm
-                ct <- ct + 1
-            }
-        })
-    }
-
+    rb <- terra::rast(rout)
+  }
+  
+  cli::cli_process_done(id = download_pb)
+  
+  if ("band_names" %in% rpostgis::dbTableInfo(con, tbl_name)$column_name) {
+    try({
+      ct <- 1
+      for (b in bands) {
+        lnm <- DBI::dbGetQuery(con, paste0("SELECT DISTINCT band_names[", 
+                                            b, "][1] as nm FROM ", nameque, " ", clauses, 
+                                            ";"))
+        names(rb)[ct] <- lnm$nm
+        ct <- ct + 1
+      }
+    })
+  }
+  
   return(rb)
-
+  
 }
 
 
@@ -263,7 +268,7 @@ getVector <- function(geom_id = NULL, layer_name = NULL, feature_name = NULL, ge
   }
   
   subquery <- paste0("WHERE")
-
+  
   if (!is.null(geom_id)) {
     subquery <- paste0(subquery,  " geom_id IN (", paste(geom_id, collapse = ", "), ") AND")
   }
