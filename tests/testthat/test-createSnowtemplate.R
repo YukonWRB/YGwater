4d85efd906b13f9d99dadfb70b580dbfd967219b
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-08-10 05:23:46 +0000

    pass tests

--- tests/testthat/test-createSnowtemplate.R
+++ tests/testthat/test-createSnowtemplate.R
@@ -5,17 +5,18 @@ test_that("all workbooks are created when a DB connection can be made", {
   # Ensure datatase connection can be made
   sk <- FALSE
   tryCatch({
-    con <- AquaConnect()
+    con <- snowConnect()
   }, error = function(e) {
     sk <<- TRUE
   })
   if (sk) {
-    skip("Could not connect to aquacache for snow workbook tests")
+    skip("Could not connect to snow survey database for snow workbook tests")
   }
   
   tempdir <- paste0(tempdir(), "/createSnowTemplateTest")
+  dir.create(tempdir, recursive = TRUE)
   on.exit(unlink(tempdir, recursive = TRUE), add = TRUE)
-  expect_no_error(suppressWarnings(createSnowTemplate("2023-03-01", circuits = "all", save_path = tempdir)))
+  expect_no_error(suppressWarnings(createSnowTemplate("2023-03-01", circuits = "all", save_path = tempdir, snowCon = con)))
   files <- list.files(tempdir)
   expect_equal(length(files), 15)
 })
