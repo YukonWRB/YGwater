f5def74734f7563e7a1053010c96f48cb3618e10
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-10-28 12:12:32 -0700

    new module

---
+++ inst/apps/YGwater/modules/admin/field/excel data entry.R
@@ -0,0 +1,14 @@
+# Module for field data entry, Excel method
+
+# Concept
+# 1. Excel template is created with predefined columns and formatting
+# 2. User fills in the template with data
+# 3. User uploads the filled template using the Shiny database app
+# Default instrument setting
+# Sampling org
+# Commissioning org
+# Purpose
+# 4. The app reads the uploaded file, validates the data, and either gives the user feedback on errors or populates a table with the parsed field data
+# 5. User can then add images, associate instruments, and associate with existing samples using actionButtons below table
+# 6. User can then submit the data to the database
+
