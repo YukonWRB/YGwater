e1e4c59abceb011c483877bcae9e7015c3ea7c61
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2025-11-03 14:19:48 -0700

    streamline RWISConnect

--- R/RWISConnect.R
+++ R/RWISConnect.R
@@ -2,7 +2,7 @@
 #'
 #' @description
 #'
-#' Establishes a connection to the RWIS (Road Weather Information System) database. Only works from within YG networks.
+#' Establishes a connection to the RWIS (Road Weather Information System) PostgreSQL database. Only works from within YG networks.
 #'
 #' @param name Database name.
 #' @param host Database host address.
@@ -16,23 +16,25 @@
 #' @export
 #'
 
-RWISConnect <- function(name = "rwdm", host = "rwis.gov.yk.ca", port = "5432", username = "rwdmread", password = "rwdmread") 
-{
-  
-  #initial checks
-  rlang::check_installed("RPostgres", reason = "Package RPostgres is required to use function RWISConnect") #This is here because RPostgreSQL is not a 'depends' of this package; it is only necessary for this function and is therefore in "suggests"
-  
-  RWIS <- DBI::dbConnect(drv = RPostgres::Postgres(),
-                         dbname = name,
-                         host = host,
-                         port = port,
-                         user = username,
-                         password = password)
-  
+RWISConnect <- function(
+  name = "rwdm",
+  host = "rwis.gov.yk.ca",
+  port = "5432",
+  username = "rwdmread",
+  password = "rwdmread"
+) {
+  RWIS <- DBI::dbConnect(
+    drv = RPostgres::Postgres(),
+    dbname = name,
+    host = host,
+    port = port,
+    user = username,
+    password = password
+  )
+
   if (!DBI::dbIsValid(RWIS)) {
     stop("Connection failed.")
   } else {
     return(RWIS)
   }
 }
-
