5ca222f42fba980e4b29292315753e65938e6476
gdelaplante <91148465+gdelaplante@users.noreply.github.com>
2026-02-27 15:51:44 -0700

    add more timeseries details for selection

--- inst/apps/YGwater/modules/admin/continuousData/addContData.R
+++ inst/apps/YGwater/modules/admin/continuousData/addContData.R
@@ -301,7 +301,7 @@ addContData <- function(id, language) {
     ts_meta <- reactiveVal({
       dbGetQueryDT(
         session$userData$AquaCache,
-        "SELECT timeseries_id, location_name AS location, parameter_name AS parameter, media_type AS media, aggregation_type AS aggregation, recording_rate AS record_rate_minutes FROM continuous.timeseries_metadata_en ORDER BY location_name, parameter_name, media_type, aggregation_type, record_rate_minutes ASC"
+        "SELECT timeseries_id, location_name AS location, alias_name AS alias, parameter_name AS parameter, media_type AS media, aggregation_type AS aggregation, recording_rate AS record_rate_minutes, sensor_priority FROM continuous.timeseries_metadata_en ORDER BY location_name, parameter_name, media_type, aggregation_type, record_rate_minutes ASC"
       )
     })
 
@@ -309,7 +309,7 @@ addContData <- function(id, language) {
     observeEvent(input$reload_module, {
       ts_meta(dbGetQueryDT(
         session$userData$AquaCache,
-        "SELECT timeseries_id, location_name AS location, parameter_name AS parameter, media_type AS media, aggregation_type AS aggregation, recording_rate AS record_rate_minutes FROM continuous.timeseries_metadata_en ORDER BY location_name, parameter_name, media_type, aggregation_type, record_rate_minutes ASC"
+        "SELECT timeseries_id, location_name AS location, alias_name AS alias, parameter_name AS parameter, media_type AS media, aggregation_type AS aggregation, recording_rate AS record_rate_minutes, sensor_priority FROM continuous.timeseries_metadata_en ORDER BY location_name, parameter_name, media_type, aggregation_type, record_rate_minutes ASC"
       ))
       moduleData$organizations <- DBI::dbGetQuery(
         session$userData$AquaCache,
